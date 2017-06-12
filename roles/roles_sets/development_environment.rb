77bfa3b073add78edbb6d2c689e81d630925876a
corselia <takiya@toran.sakura.ne.jp>
2017-06-12 17:28:03 +0900

    [update] comment out about vagrant
    vagrant by apt-get is too old so remove

--- roles/roles_sets/development_environment.rb
+++ roles/roles_sets/development_environment.rb
@@ -1,3 +1,6 @@
 include_recipe "../../cookbooks/docker/docker.rb"
 include_recipe "../../cookbooks/virtualbox/virtualbox.rb"
-include_recipe "../../cookbooks/vagrant/vagrant.rb"
+
+# version is too old...
+# ref: https://www.vagrantup.com/downloads.html
+# include_recipe "../../cookbooks/vagrant/vagrant.rb"
