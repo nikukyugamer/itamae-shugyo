4263cff982df18758167b0a92b4fbbc16a6de2e7
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-01-11 15:09:56 +0900

    Refactoring

--- remote_files/aws_centos/chsh_to_zsh.sh
+++ remote_files/aws_centos/chsh_to_zsh.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -xe
 
 echo "/usr/bin/zsh" | sudo tee -a /etc/shells
-
-expect -c "
-spawn sudo chsh centos
-expect \"New shell\"
-send \"\/usr\/bin\/zsh\\n\"
-"
+sudo chsh centos -s /usr/bin/zsh
 
 exit 0
